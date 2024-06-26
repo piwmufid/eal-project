7f2ed90ef4280364912f3db3bee9a4f37608eb75
ilzammahbobi28 <ilzammahbobi28@gmail.com>
2024-06-26 05:58:36 +0000

    commit kedua dari project idx

---
+++ storage/framework/views/9b9c85b4fb58874caca44cd4827b3996.php
@@ -0,0 +1,52 @@
+<div class="container-xxl py-5">
+    <div class="container">
+        <h5 class="section-title mb-sm-3 ff-secondary text-start text-primary fw-normal">Classes</h5>
+        <div class="row g-4">
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="shadow service-item rounded pt-3">
+                    <a href="/speaking">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
+                            <h5>Speaking Class</h5>
+                            <p>The class focuses on speaking ability and public speaking, regularly for beginers</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="shadow service-item rounded pt-3">
+                    <a href="/listening">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-assistive-listening-systems text-primary mb-4"></i>
+                            <h5>Listening Class</h5>
+                            <p>The class takes the focus on English comprehension conversation practice.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="shadow service-item rounded pt-3">
+                    <a href="/reading">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-book-open text-primary mb-4"></i>
+                            <h5>Reading Class</h5>
+                            <p>The class gives comprehension of English grammar structure, including reading skill.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                <div class="shadow service-item rounded pt-3">
+                    <a href="/translation">
+                        <div class="p-4">
+                            <i class="fa fa-3x fa-language text-primary mb-4"></i>
+                            <h5>Translation Class</h5>
+                            <p>The class trains the members the ability to translate and write English literature.</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php /**PATH /home/user/eal-project/resources/views/components/home/kelas.blade.php ENDPATH**/ ?>
